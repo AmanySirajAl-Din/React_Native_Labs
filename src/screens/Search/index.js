418c7415e317f2e19763bf81f3268343d06ee158
Amany Siraj Al-Din <amanysirajaldin@email.com>
2021-11-06 01:13:55 +0200

    Import 'SearchBar' component

--- src/screens/Search/index.js
+++ src/screens/Search/index.js
@@ -1,13 +1,14 @@
 // import axios from 'axios';
 import React from 'react';
 import {View} from 'react-native';
+import {SearchBar} from '../../components';
 
 import styles from './styles';
 
 export default function Search() {
   return (
     <View style={styles.container}>
-      <Text>Hello Search</Text>
+      <SearchBar />
     </View>
   );
 }
